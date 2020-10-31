cb37f5af4657c51efc3ff5e93bb9e4a6751e18d5
Kyle Tong <kyle.simon.tong@gmail.com>
2020-10-31 12:24:02 +0900

    Make the pricing page a bit better on mobile

--- src/components/PricingTile.vue
+++ src/components/PricingTile.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="w-3/4 md:w-2/3 py-2">
-    <div class="bg-white p-4">
+  <div class="w-10/12 md:w-2/3 py-2 flex flex-col">
+    <div class="bg-white p-4 flex flex-col">
       <div class="text-center border border-black p-4 my-2">
         <h3 class="text-2xl">
           {{ pricing.Title }}
         </h3>
       </div>
-      <h3 class="text-xl text-center md:text-left md:text-3xl">
+      <h3 class="text-xl text-center lg:text-left md:text-3xl">
         {{ pricing.Price }}
       </h3>
-      <div class="w-full flex">
-        <div class="w-1/2 md:pr-8" v-if="this.pricing.Image">
+      <div class="w-full flex flex-col lg:flex-row">
+        <div class="w-full lg:w-1/2 lg:pr-8" v-if="this.pricing.Image">
           <img :src="`${imageBaseUrl}${this.pricing.Image.url}`" :alt="pricing.title">
         </div>
-        <div class="flex md:flex-col w-1/2">
-          <VueShowdown :markdown="pricing.Description" class="list-disc pl-2 md:pl-8 text-xs md:text-sm" />
+        <div class="flex flex-col w-full lg:w-1/2">
+          <VueShowdown :markdown="pricing.Description" class="list-disc pl-2 lg:pl-8 text-xs pt-2 lg:pt-0 md:text-sm" />
           <LinkButton :link="this.questionnaireButton" :linkType="light" class="pt-8 pl-2 md:pl-8" />
         </div>
       </div>
--- src/components/RouteTitle.vue
+++ src/components/RouteTitle.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="self-end w-2/3 text-white">
-    <h1 class="text-4xl">{{ title }}</h1>
+    <h1 class="text-4xl text-center md:text-left">{{ title }}</h1>
   </div>
 </template>
 
