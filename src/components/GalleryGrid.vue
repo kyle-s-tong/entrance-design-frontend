8c42df422f209081d9cfff08d19508f37deb7670
Kyle Tong <kyle.simon.tong@gmail.com>
2020-11-01 16:42:41 +0900

    Add click handler on gallery so the route refreshes properly

--- src/components/GalleryGrid.vue
+++ src/components/GalleryGrid.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex w-full px-10 grid grid-cols-1 md:grid-cols-3 gap-2 justify-center">
     <div v-for="gallery in galleries" :key="gallery.Title" class="border-2 border-white relative flex">
-      <router-link :to="`/gallery/${gallery.id}`" class="md:flex justify-center w-full">
+      <router-link :to="`/gallery/${gallery.id}`" class="md:flex justify-center w-full" v-on:click.native="refreshGalleryRoute">
         <img v-if="gallery.MainImage && gallery.MainImage.url" :src="`${imageBaseUrl}${gallery.MainImage.url}`" alt="" class="w-full">
         <p class="absolute bottom-0 text-white bg-black bg-opacity-50 my-2 mx-1 p-1">{{ gallery.Title }}</p>
       </router-link>
@@ -20,6 +20,11 @@ export default {
   computed: {
     imageBaseUrl: function () {
       return getImageUrl();
+    }
+  },
+  methods: {
+    refreshGalleryRoute: function () {
+      this.$router.go();
     }
   }
 }
